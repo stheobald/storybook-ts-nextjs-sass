6293b8d1c818776b91c7785dcfd185cadebcd206
Michael Shilman <michael@lab80.co>
2020-03-30 15:51:02 +0800

    Global SCSS

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/preset-typescript",
-    //"@storybook/preset-scss"
     {
       name: "@storybook/preset-scss",
       options: {
@@ -16,20 +15,16 @@ module.exports = {
         }
       }
     }
-  ]
-  // webpackFinal: async (config, { configType }) => {
-  //   // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  //   // You can change the configuration based on that.
-  //   // 'PRODUCTION' is used when building the static version of storybook.
-
-  //   // Make whatever fine-grained changes you need
-  //   config.module.rules.push({
-  //     test: /\.scss$/,
-  //     use: ["style-loader", "css-loader", "sass-loader"],
-  //     include: path.resolve(__dirname, "../")
-  //   });
-
-  //   // Return the altered config
-  //   return config;
-  // }
+  ],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      enforce: "pre",
+      test: /.scss$/,
+      loader: "sass-resources-loader",
+      options: {
+        resources: ["./src/styles/fonts.scss", "./src/styles/theme.scss"]
+      }
+    });
+    return config;
+  }
 };
